6c88ea30d205fabe0bbb03e3243b00a297421dee
Blake Smith <blake.smith.mn@gmail.com>
2022-09-08 20:42:26 -0500

    initial setup

---
+++ server/public/scripts/client.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+console.log( 'js' );
+
+$( document ).ready( function(){
+  console.log( 'JQ' );
+
+}); // end doc ready
+
+function clickListeners() {
+  
+}
---
+++ server/routes/tasks.router.js
@@ -0,0 +1,18 @@
+// BOILERPLATE
+const {Router} = require('express');
+const express = require('express');
+const tasksRouter = express.Router();
+
+// connecting to the database
+const pool = require('../modules/pool.js');
+
+// GET
+
+// POST
+
+// PUT / UPDATE
+
+// DELETE
+
+
+module.exports = tasksRouter;
